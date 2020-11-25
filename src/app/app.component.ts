299eb8760ab5a5173c671e2e1cc9fa473288a60e
FazioNico <contact@nicolasfazio.ch>
2020-11-25 13:56:40 +0100

    upd: demo with Observable & BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Plugins } from '@capacitor/core';
+import { Motion, Plugins } from '@capacitor/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,40 @@ import { Plugins } from '@capacitor/core';
 })
 export class AppComponent {
   title = 'CapacitorDemo';
+  alpha: number = 0;
+  datas: any;
+  private _datas$: BehaviorSubject<any> = new BehaviorSubject(null);
+  datas$: Observable<any> = this._datas$.asObservable().pipe(
+    // manipulation de donnée avec les opérateur RxJs
+    // map(datas =>  {
+    //   return {
+    //     ...datas,
+    //     alpha: datas?.alpha + '°'
+    //   }
+    // })
+  );
+
+  async runMotion(btnOn: HTMLIonButtonElement, btnOff: HTMLIonButtonElement) {
+    console.log(btnOff);
+    btnOn.disabled = true;
+    btnOff.disabled = false;
+    Motion.addListener('orientation', (event) => {
+      console.log('-->', event);
+      this.alpha = event?.alpha;
+      const data = {
+        alpha: event?.alpha,
+        beta: event?.beta,
+        gamma: event?.gamma
+      }
+      this.datas = data;
+      this._datas$.next(data);
+    });
+
+  }
+
+  stopMotion(btnOn: HTMLIonButtonElement, btnOff: HTMLIonButtonElement) {
+    btnOn.disabled = false;
+    btnOff.disabled = true;
+    Motion.removeAllListeners();
+  }
 }
